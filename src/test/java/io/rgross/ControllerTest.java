3ca23c0dbe7d008b06be81ea6ff01d383ee4057c
wmattern0 <wm.m.mattern@gmail.com>
2017-03-17 13:09:24 -0400

    working endpoint test

--- src/test/java/io/rgross/ControllerTest.java
+++ src/test/java/io/rgross/ControllerTest.java
@@ -4,10 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.client.RequestMatcher;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,36 +28,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by ryan on 3/16/17.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@Configuration("servlet-context.xml")
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    TestController testController;
-
-
     private MockMvc mockMvc;
-
-
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-        this.testController = new TestController();
-    }
-
 
     @Test
     public void helloWorldTest() throws Exception {
-        String x = "HelloWorld!";
-        MockHttpServletRequestBuilder query = get("/hello").contentType(MediaType.APPLICATION_JSON);
-       // mockMvc.perform(query).andDo(MockMvcResultHandlers.print());
-        mockMvc.perform(query).andExpect(status().is4xxClientError());
-      //  mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-        System.out.println(testController.helloWorld());
+        MockHttpServletRequestBuilder query = get("/hello");
+        mockMvc.perform(query).andExpect(status().isOk());
     }
 }
